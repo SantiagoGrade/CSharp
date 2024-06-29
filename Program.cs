313e23c8228943e975cd8e4cbe5c65e0445fab32
Zergrade <santiago54911@gmail.com>
2024-06-29 18:11:04 -0500

    Update

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿Random random = new Random ();
-int dayUntilExpiration = 11;
+int dayUntilExpiration = 10;
 int discountPercentage = 0;
 
 if (dayUntilExpiration <= 10)
@@ -8,19 +8,17 @@ if (dayUntilExpiration <= 10)
 }
 else if(dayUntilExpiration <= 5)
 {
-    Console.WriteLine("Your subscription expires in _ days.\n Renew now and save 10%!");
+    Console.WriteLine($"Your subscription expires in {dayUntilExpiration} days.\n Renew now and save 10%!");
     discountPercentage = 10;
 }
 else if(dayUntilExpiration == 1)
 {
-    Console.WriteLine("Your subscription expires within a day! \n Renew now and save 20%");
+    
     discountPercentage = 20;
+    Console.WriteLine($"Your subscription expires {dayUntilExpiration} within a day! \n Renew now and save {discountPercentage}%");
 }
 else if(dayUntilExpiration == 0)
 {
     Console.WriteLine("Your subscription has expired.");
 }
-else
-{
 
-}
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1a8739b6895867c005c79b917fc57432b5b586c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2d47874cef67086519cc6dbbb69de883ce413fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
