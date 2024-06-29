a47cd5b109e2b9f29bf87c8f23f8a34d801307a2
Zergrade <santiago54911@gmail.com>
2024-06-28 20:38:21 -0500

    Update

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8034ab7bdaabbde395d01f90301709ac4c808a90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd0e2da12af9dd38cf067c490bdbf4b4db29e220")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
