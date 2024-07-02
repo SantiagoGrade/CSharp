857b3903dcf9b73b43403459fd9c49256b3cad3e
Zergrade <santiago54911@gmail.com>
2024-07-01 23:38:36 -0500

    Update

--- Program.cs
+++ Program.cs
@@ -1,25 +1,4 @@
-﻿/*
-string[] faraudulentOrderIDs = new string[3];
-
-faraudulentOrderIDs[0] = "A123";
-faraudulentOrderIDs[1] = "B456";
-faraudulentOrderIDs[2] = "C789";
-
-
-string[] faraudulentOrderIDs = {"A123", "B456", "C789"};
-
-Console.WriteLine($"First: {faraudulentOrderIDs[0]}");
-Console.WriteLine($"Second: {faraudulentOrderIDs[1]}");
-Console.WriteLine($"Third: {faraudulentOrderIDs[2]}");
-
-faraudulentOrderIDs[0] = "F000";
-
-Console.WriteLine($"Reassign First: {faraudulentOrderIDs[0]}");
-
-Console.WriteLine($"There are {faraudulentOrderIDs.Length} fraudulent orders to procces.");
-*/
-
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 
 string[] names = {"Rowena", "Robin", "Bao"};
 foreach (string name in names)
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2002045547e30e799c5f34fbb1c1026d1a0afebe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4f383f77c00f970a9f75249a745eadbcebeb3a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
