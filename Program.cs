3e383f61c188cb18a490b5dfa45f7bf9b86e721a
Zergrade <santiago54911@gmail.com>
2024-07-08 19:02:40 -0500

    Update

--- Program.cs
+++ Program.cs
@@ -1,9 +1,4 @@
-﻿/*
-  The following code creates five random OrderIDs
-  to test the fraud detection process.  OrderIDs 
-  consist of a letter from A to E, and a three
-  digit number. Ex. A123.
-*/
+﻿
 Random random = new Random();
 string[] orderIDs = new string[5];
 
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbc2c2809475b3009e5b441f1b087ff38708f1a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc5bc9b48ad581b905e4bfb2360309aa550824d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
